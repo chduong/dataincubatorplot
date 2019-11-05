c1b91b36c89f3202e4a3919cf2a521bfa163b68d
chduong <chduong@users.noreply.github.com>
2019-11-05 01:30:20 -0500

    Update btcdata.py

--- btcdata.py
+++ btcdata.py
@@ -1,9 +1,10 @@
 import numpy as np
+from numpy import genfromtxt
 np.set_printoptions(precision=10) #sets np precision to 10
 
 import matplotlib.pyplot as plt
 
-from scipy import stats
+import seaborn as sns
 
 import pandas as pd
 pd.set_option('precision', 10) #sets pandas precision to 10
@@ -12,6 +13,7 @@ pd.set_option('display.precision', 10) #sets pandas display precision to 10
 pd.set_option('display.max_rows', 10)
 pd.set_option('display.max_columns', 500)
 pd.set_option('display.width', 1000)
+pd.plotting.register_matplotlib_converters()
 
 def header(msg):
     print('-' * 50)
@@ -20,26 +22,47 @@ def header(msg):
 filename = 'btcdata_2017-01-01-2019-11-04.csv'
 df = pd.read_csv(filename)
 df['Date'] = pd.to_datetime(df['Date'])
-df2 = df.set_index(['Date'])
-df2 = df2.loc['2019-10-01':'2019-10-31']
+df = df[['Date', 'Price']]
+df = df.set_index(['Date'])
+df['Price'] = df['Price'].str.replace(',', '')
+df = df.sort_values(by='Date') #sorting is optional
+# print(df)
 
-print(df2)
-
-
-header('Plot 1')
-# print(len(df))
+header('Plot 1, Federal Interest Rates Changed at Red Line')
 #interest rate 1 Sept 18 2019
-previousfir1 = 2.25
-fir1 = 2
+# previous federal interest rate = 2.25
+# new federal interest rate = 2
+df2 = df.loc['10/01/2019':'10/30/2019']
+filename2 = 'data2.csv'
+df2.to_csv(filename2)
+df2 = pd.read_csv(filename2)
 
-##interest rate 2 Oct 30 2019
-previousfir2 = fir1
-fir2 = 1.75
+df3 = df.loc['10/01/2019':'11/4/2019']
+filename3 = 'data3.csv'
+df3.to_csv(filename3)
+df3 = pd.read_csv(filename3)
+# plot2 = df3.plot(x='Date', y='Price', color='red')
 
-print(df.dtypes)
-# df = df.set_index(['Date'])
-# print(df2)
-# df3 = df2.loc['Oct 1, 2018':'Oct 31, 2018']
-
-# df2.plot(kind='scatter', x='Date', y='Change %', color='red')
+ax = df3.plot(x='Date', y='Price', color= 'red')
+df2.plot(ax=ax, x='Date', y='Price', color='blue')
 # plt.show()
+
+header('Plot 2, Federal Interest Rates Changed at Red Line')
+##interest rate 2 Oct 30 2019
+# previous federal interest rate = 2.00
+# new federal interest rate = 1.75
+
+df4 = df.loc['08/15/2019':'09/17/2019']
+filename4 = 'data4.csv'
+df4.to_csv(filename4)
+df4 = pd.read_csv(filename4)
+
+df5 = df.loc['08/15/2019':'10/30/2019']
+filename5 = 'data5.csv'
+df5.to_csv(filename5)
+df5 = pd.read_csv(filename5)
+# plot2 = df3.plot(x='Date', y='Price', color='red')
+
+ax2 = df5.plot(x='Date', y='Price', color= 'red')
+df4.plot(ax=ax2, x='Date', y='Price', color='blue')
+plt.show()
